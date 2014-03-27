1ba910cdfd18dbf68cd4af7b493ff7efcda6dcd4
Daniel Hauagge <daniel.hauagge@gmail.com>
2014-03-26 20:02:18 -0400

    Super pixels seem to be working

--- compute_saliency.cpp
+++ compute_saliency.cpp
@@ -2,6 +2,30 @@
 #include "opencl.hpp"
 #include "slicsuperpixels.hpp"
 
+void
+writePgm(const std::string &outFName, int *data, const Size &size)
+{
+    std::ofstream f(outFName.c_str());
+
+    int maxVal = data[0];
+    for(int i = 1; i < size.width * size.height; i++) {
+        maxVal = (maxVal > data[i]) ? maxVal : data[i];
+    }
+
+    f << "P2\n"
+      << size.width << " " << size.height << "\n"
+      << maxVal << "\n";
+
+    for (int i = 0, k = 0; i < size.height; i++) {
+        std::string sep = "";
+        for (int j = 0; j < size.width; j++, k++) {
+            f << sep << data[k];
+            sep = " ";
+        }
+        f << "\n";
+    }
+}
+
 /*******************************************************************************
  * Main
  *******************************************************************************/
@@ -9,11 +33,16 @@ int
 main(int argc, char const *argv[])
 {
     std::string inFName = argv[1];
-    Image img(inFName);
-    int superPixelSpacing = 30;
-    int nIters = 3;
+    std::string outFName = argv[2];
 
-    slicSuperPixels(img, superPixelSpacing, nIters);
+    Image img(inFName);
+    int superPixelSpacing = 64;
+    int nIters = 10;
+
+    int clusterAssig[img.size().width * img.size().height];
+    slicSuperPixels(img, superPixelSpacing, nIters, clusterAssig);
+
+    writePgm(outFName, clusterAssig, img.size());
 
     return EXIT_SUCCESS;
 }
--- test_image.cpp
+++ test_image.cpp
@@ -7,6 +7,12 @@ main(int argc, char const *argv[])
     std::string outFName = argv[2];
 
     Image img(inFName);
+
+    LOG_EXPR(img.stride());
+    LOG_EXPR(img.size().width * sizeof(float) * 3);
+
+    for(int i = 0; i < img.size().width * img.size().height * 3; i++) LOG_INFO(img(0, 0)[i] * 255);
+
     img.writeToFile(outFName);
 
     return EXIT_SUCCESS;

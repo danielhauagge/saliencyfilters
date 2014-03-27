81186e48076382c5f1f37f136d0b3698e4bd6640
Daniel Hauagge <daniel.hauagge@gmail.com>
2014-03-26 20:31:25 -0400

    Made dist metric match paper

--- compute_saliency.cpp
+++ compute_saliency.cpp
@@ -38,9 +38,10 @@ main(int argc, char const *argv[])
     Image img(inFName);
     int superPixelSpacing = 64;
     int nIters = 10;
+    float relWeight = 0.5;
 
     int clusterAssig[img.size().width * img.size().height];
-    slicSuperPixels(img, superPixelSpacing, nIters, clusterAssig);
+    slicSuperPixels(img, superPixelSpacing, nIters, relWeight, clusterAssig);
 
     writePgm(outFName, clusterAssig, img.size());
 
